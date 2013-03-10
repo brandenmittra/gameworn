e11ad00ce70843425cbcdcc0fc6ec6d50d919566
Branden Mittra <branden@univrs.co.uk>
2013-03-10 02:39:50 +0000

    Add Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Gameworn::Application.routes.draw do
+  get 'about' => 'pages#about'
+
+  root :to => 'pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
