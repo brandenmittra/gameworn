acfc41b72185cd36bb4912bbae997aa634c313f9
Branden Mittra <branden@univrs.co.uk>
2013-03-10 01:01:02 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gameworn::Application.config.secret_token = '02b2a7a16ff3b1d28d9f75a27605e6edbe1550bda2d56590b70ee1f75e50d5c03a39e85ee41b5748a2a96994802c9e9f6925d534d620ffee7734afe769809bff'
