6e1366ef8037f1801ebc75d5efd4efe59c63e233
Branden Mittra <branden@univrs.co.uk>
2013-03-10 03:18:00 +0000

    Add Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,8 @@ Gameworn::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Add to production.rb as well
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
 end
